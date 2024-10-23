fa0de82a8d4139f3d8a389eb7a3da08eda8b1eeb
Ömer Faruk Sezen <104766543+mexikali@users.noreply.github.com>
2024-10-23 23:54:21 +0300

    template engine is created

--- PCAT/app.js
+++ PCAT/app.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const path = require('path');
+const ejs = require('ejs');
+
 const app = express();
+
+// Template Engine
+app.set('view engine', 'ejs');
+
+// Middleware
 app.use(express.static('public'));
 
 /*
@@ -11,9 +18,19 @@ const myLogger = (req, res, next) => {
 app.use(myLogger);
 */
 
+// Routes
 app.get('/', (req,res) => {
-    res.sendFile(path.resolve(__dirname, 'public/img/about-1.jpg'));
-})
+    //res.sendFile(path.resolve(__dirname, 'public/img/about-1.jpg'));
+    res.render('index');
+});
+
+app.get('/about', (req,res) => {
+    res.render('about');
+});
+
+app.get('/add', (req,res) => {
+    res.render('add');
+});
 
 const port = 3000;
 app.listen(port,() => {
