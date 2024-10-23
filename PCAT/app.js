37c9a9bae6ac2f74885f3fa5f0618214b19b2829
Ömer Faruk Sezen <104766543+mexikali@users.noreply.github.com>
2024-10-23 23:11:09 +0300

    static files are added

--- PCAT/app.js
+++ PCAT/app.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+app.use(express.static('public'));
+
+/*
+const myLogger = (req, res, next) => {
+    console.log("Middleware is run");
+    next();
+}
+app.use(myLogger);
+*/
 
 app.get('/', (req,res) => {
-    res.send("MERHABA");
+    res.sendFile(path.resolve(__dirname, 'public/img/about-1.jpg'));
 })
 
 const port = 3000;
