298b01be35241fb1ae4e2bd4cc57e48edfca01fe
Ömer Faruk Sezen <104766543+mexikali@users.noreply.github.com>
2024-10-26 22:33:57 +0300

    partials folder is created

--- homeworks/clean-blog/app.js
+++ homeworks/clean-blog/app.js
@@ -1,5 +1,14 @@
 const express = require('express');
+const path = require('path');
+const ejs = require('ejs');
+
 const app = express();
+
+// Template Engine
+app.set('view engine', 'ejs');
+
+// Middleware
+app.use(express.static('public'));
 
 app.get('/', (req,res) => {
     const blog = { id: 1, title: "Blog title", description: "Blog description" };
