07685931de27baa258a527d2d111d9f5843c5f46
Ömer Faruk Sezen <104766543+mexikali@users.noreply.github.com>
2024-10-26 22:41:11 +0300

    clean-blog project second part is completed

--- homeworks/clean-blog/app.js
+++ homeworks/clean-blog/app.js
@@ -10,10 +10,22 @@ app.set('view engine', 'ejs');
 // Middleware
 app.use(express.static('public'));
 
+// Routess
 app.get('/', (req,res) => {
-    const blog = { id: 1, title: "Blog title", description: "Blog description" };
-    res.send(blog);
-})
+    res.render('index');
+});
+
+app.get('/about', (req,res) => {
+    res.render('about');
+});
+
+app.get('/add_post', (req,res) => {
+    res.render('add_post');
+});
+
+app.get('/post', (req,res) => {
+    res.render('post');
+});
 
 const port = 3000;
 app.listen(port,() => {
