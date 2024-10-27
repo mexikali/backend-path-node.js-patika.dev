ff11f0be56c1dca2bb1cc56e2794131903bccaaf
Ömer Faruk Sezen <104766543+mexikali@users.noreply.github.com>
2024-10-27 23:25:11 +0300

    clean-blog project third part is completed

--- homeworks/clean-blog/app.js
+++ homeworks/clean-blog/app.js
@@ -1,18 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
 const ejs = require('ejs');
+const Blog = require('./models/Blog');
 
 const app = express();
+
+// connect DB
+mongoose.connect('mongodb://localhost/cleanblog-test-db');
 
 // Template Engine
 app.set('view engine', 'ejs');
 
 // Middleware
 app.use(express.static('public'));
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 
 // Routess
-app.get('/', (req,res) => {
-    res.render('index');
+app.get('/', async (req,res) => {
+    const blogs = await Blog.find({});
+    res.render('index', {blogs});
 });
 
 app.get('/about', (req,res) => {
@@ -25,6 +33,11 @@ app.get('/add_post', (req,res) => {
 
 app.get('/post', (req,res) => {
     res.render('post');
+});
+
+app.post('/blogs', async (req,res) => {
+    await Blog.create(req.body);
+    res.redirect('/');
 });
 
 const port = 3000;
