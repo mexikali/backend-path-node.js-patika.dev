3312ca681a17f79a00ede210d8fdc0ebf44f04ad
Ömer Faruk Sezen <104766543+mexikali@users.noreply.github.com>
2024-10-31 00:36:54 +0300

    clean-blog project fifth part is done

--- homeworks/clean-blog/app.js
+++ homeworks/clean-blog/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const path = require('path');
 const ejs = require('ejs');
 const Blog = require('./models/Blog');
+const blogController = require('./controllers/blogController');
+const pageController = require('./controllers/pageController');
 
 const app = express();
 
@@ -16,34 +19,21 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+app.use(methodOverride('_method', {
+    methods: ['POST', 'GET']
+}));
 
 // Routess
-app.get('/', async (req,res) => {
-    const blogs = await Blog.find({});
-    res.render('index', {blogs});
-});
+app.get('/', blogController.getAllBlogs);
+app.get('/posts/:id', blogController.getBlog);
+app.post('/blogs', blogController.createBlog);
+app.put('/blogs/:id', blogController.updateBlog);
+app.delete('/blogs/:id', blogController.deleteBlog);
 
-app.get('/posts/:id', async (req,res) => {
-    const blog = await Blog.findById(req.params.id);
-    res.render('post', {blog});
-});
+app.get('/about', pageController.getAboutPage);
+app.get('/add_post', pageController.getAddPage);
+app.get('/blogs/edit/:id', pageController.getEditPage);
 
-app.get('/about', (req,res) => {
-    res.render('about');
-});
-
-app.get('/add_post', (req,res) => {
-    res.render('add_post');
-});
-
-app.get('/post', (req,res) => {
-    res.render('post');
-});
-
-app.post('/blogs', async (req,res) => {
-    await Blog.create(req.body);
-    res.redirect('/');
-});
 
 const port = 3000;
 app.listen(port,() => {
