2a7e52c1ddfe335651db6e26221322c24b210ac7
Ömer Faruk Sezen <104766543+mexikali@users.noreply.github.com>
2024-10-31 22:58:16 +0300

    smartEdu - DB connection and model creation are done

--- smartEdu/app.js
+++ smartEdu/app.js
@@ -1,7 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const pageRoute = require('./routes/pageRoute');
+const courseRoute = require('./routes/courseRoute');
 
 const app = express();
+
+// Connect DB
+mongoose.connect('mongodb://localhost/smartedu-db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+}).then(() => {
+    console.log('DB CONNECTED SUCCESSFULY');
+}).catch(() => {
+    console.log('ERROR OCCURRED WHILE CONNECTING TO DB');
+});
 
 // Template Engine
 app.set("view engine", "ejs");
@@ -11,6 +25,7 @@ app.use(express.static("public"));
 
 // Routes
 app.use('/', pageRoute);
+app.use('/courses', courseRoute);
 
 const port = 3000;
 app.listen(port,() => {
