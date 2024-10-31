a5d23608a0b4a6deffb14c13dde5554537fcbfef
Ömer Faruk Sezen <104766543+mexikali@users.noreply.github.com>
2024-10-31 22:12:48 +0300

    smartEdu - temp files are created

--- smartEdu/app.js
+++ smartEdu/app.js
@@ -2,8 +2,18 @@ const express = require('express');
 
 const app = express();
 
+// Template Engine
+app.set("view engine", "ejs");
+
+// Middlewares
+app.use(express.static("public"));
+
 app.get('/', (req,res) => {
-    res.status(201).send('INDEX SAYFASI');
+    res.status(200).render('index', {page_name: 'index'});
+});
+
+app.get('/about', (req,res) => {
+    res.status(200).render('about', {page_name: 'about'});
 });
 
 const port = 3000;
