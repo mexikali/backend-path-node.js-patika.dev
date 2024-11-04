c88fbcce21f36031a1bd4e3b5fc443a9decfc59b
Ömer Faruk Sezen <104766543+mexikali@users.noreply.github.com>
2024-11-04 23:09:46 +0300

    smartEdu - session

--- smartEdu/controllers/authController.js
+++ smartEdu/controllers/authController.js
@@ -26,7 +26,8 @@ exports.loginUser = async (req, res) => {
         if(user) {
             bcrypt.compare(password, user.password, (err, same) => {
                 if ( same ){
-                    res.status(200).send('YOU ARE LOGGED IN !!!');
+                    req.session.userID = user._id;
+                    res.status(200).redirect('/');
                 }
             });
         } 
