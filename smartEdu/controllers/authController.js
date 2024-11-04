a8053c01e2863d36a6e35fd2dd68b53307f7e15a
Ömer Faruk Sezen <104766543+mexikali@users.noreply.github.com>
2024-11-04 23:36:54 +0300

    smartEdu - dashboard route

--- smartEdu/controllers/authController.js
+++ smartEdu/controllers/authController.js
@@ -27,7 +27,7 @@ exports.loginUser = async (req, res) => {
             bcrypt.compare(password, user.password, (err, same) => {
                 if ( same ){
                     req.session.userID = user._id;
-                    res.status(200).redirect('/');
+                    res.status(200).redirect('/users/dashboard');
                 }
             });
         } 
@@ -43,4 +43,9 @@ exports.logoutUser = async (req, res) => {
     req.session.destroy(() => {
         res.redirect('/');
     })
+};
+
+exports.getDashboardPage = async (req,res) => {
+    const user = await User.findOne({_id: req.session.userID});
+    res.status(200).render('dashboard', {page_name: 'dashboard', user});
 };
