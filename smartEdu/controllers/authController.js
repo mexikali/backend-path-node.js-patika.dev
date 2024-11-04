0130a088bd20b98a15b62381e05359ac9f2b82fe
Ömer Faruk Sezen <104766543+mexikali@users.noreply.github.com>
2024-11-04 22:43:27 +0300

    smartEdu - login op

--- smartEdu/controllers/authController.js
+++ smartEdu/controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
 exports.createUser = async (req, res) => {
 
@@ -9,6 +10,26 @@ exports.createUser = async (req, res) => {
             status: 'success',
             user
         });
+    } catch (error){
+        res.status(400).json({
+            status: 'fail',
+            error
+        });
+    }
+};
+
+exports.loginUser = async (req, res) => {
+
+    try {
+        const {email, password} = req.body;
+        const user = await User.findOne({email});
+        if(user) {
+            bcrypt.compare(password, user.password, (err, same) => {
+                if ( same ){
+                    res.status(200).send('YOU ARE LOGGED IN !!!');
+                }
+            });
+        } 
     } catch (error){
         res.status(400).json({
             status: 'fail',
