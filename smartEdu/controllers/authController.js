9ec4da93557601548c8f1d3f7c347e63e2a72b5c
Ömer Faruk Sezen <104766543+mexikali@users.noreply.github.com>
2024-11-05 21:09:10 +0300

    smartEdu - adding course

--- smartEdu/controllers/authController.js
+++ smartEdu/controllers/authController.js
@@ -1,5 +1,6 @@
-const User = require('../models/User');
 const bcrypt = require('bcrypt');
+const User = require('../models/User');
+const Category = require('../models/Category');
 
 exports.createUser = async (req, res) => {
 
@@ -44,5 +45,6 @@ exports.logoutUser = async (req, res) => {
 
 exports.getDashboardPage = async (req,res) => {
     const user = await User.findOne({_id: req.session.userID});
-    res.status(200).render('dashboard', {page_name: 'dashboard', user});
+    const categories = await Category.find();
+    res.status(200).render('dashboard', {page_name: 'dashboard', user, categories});
 };
