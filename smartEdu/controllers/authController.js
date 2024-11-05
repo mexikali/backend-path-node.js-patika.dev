aca0bd34766f7a33cc2033be4818097f0c069178
Ömer Faruk Sezen <104766543+mexikali@users.noreply.github.com>
2024-11-05 21:38:32 +0300

    smartEdu -> course-teacher relation

--- smartEdu/controllers/authController.js
+++ smartEdu/controllers/authController.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcrypt');
 const User = require('../models/User');
 const Category = require('../models/Category');
+const Course = require('../models/Course');
 
 exports.createUser = async (req, res) => {
 
@@ -46,5 +47,8 @@ exports.logoutUser = async (req, res) => {
 exports.getDashboardPage = async (req,res) => {
     const user = await User.findOne({_id: req.session.userID});
     const categories = await Category.find();
-    res.status(200).render('dashboard', {page_name: 'dashboard', user, categories});
+    const courses = await Course.find({
+        user: req.session.userID
+    });
+    res.status(200).render('dashboard', {page_name: 'dashboard', user, categories, courses});
 };
