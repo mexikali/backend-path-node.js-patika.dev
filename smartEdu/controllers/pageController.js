c2a6dc1df18839cf0ca1680174791153ee67ea5d
Ömer Faruk Sezen <104766543+mexikali@users.noreply.github.com>
2024-11-08 22:43:15 +0300

    smartEdu - sending mail in contact page

--- smartEdu/controllers/pageController.js
+++ smartEdu/controllers/pageController.js
@@ -1,3 +1,5 @@
+const nodemailer = require('nodemailer');
+
 exports.getIndexPage = (req,res) => {
     res.status(200).render('index', {page_name: 'index'});
 };
@@ -16,4 +18,38 @@ exports.getLoginPage = (req,res) => {
 
 exports.getContactPage = (req,res) => {
     res.status(200).render('contact', {page_name: 'contact'});
+};
+
+exports.sendEmail = async (req,res) => {
+    const outputMessage = `
+    <h1>Message Details </h1>
+    <ul>
+        <li> Name: ${req.body.name}</li>
+        <li> Email: ${req.body.email}</li>
+    </ul>
+    <h1>Message</h1>
+    <p>${req.body.message}</p>
+    `;
+
+    const transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true, // true for port 465, false for other ports
+        auth: {
+          user: "omerfaruksezenn@gmail.com",
+          pass: "nuvmkgsmryeiqsse",
+        },
+      });
+      
+    const info = await transporter.sendMail({
+          from: '"Smart EDU Contact Form" <omerfaruksezenn@gmail.com>', // sender address
+          to: "omer.sezen@metu.edu.tr", // list of receivers
+          subject: "Smart Edu Contact Page New Message ✔", // Subject line
+          text: "Hello world?", // plain text body
+          html: outputMessage, // html body
+    });
+      
+    console.log("Message sent: %s", info.messageId);
+
+    res.status(200).redirect('/contact');
 };
