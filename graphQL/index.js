c4465e7f6cd30c994abde2ade78fd6944e27a5ff
Ömer Faruk Sezen <104766543+mexikali@users.noreply.github.com>
2024-11-13 22:05:49 +0300

    graphQL - custom types

--- graphQL/index.js
+++ graphQL/index.js
@@ -1,26 +1,38 @@
 const { ApolloServer, gql } = require('apollo-server');
 const { ApolloServerPluginLandingPageGraphQLPlayground } = require('apollo-server-core');
+
+const author = {id:"1", name:"Albert", surname:"Camus", age:50, books:[{id:"1", title:"Test Title", score:9, isPublished:false}, {id:"2", title:"Deneme Title", score:5, isPublished:true}]};
+const book = {id: "sdk646dsad3d4sa4d", title: 'Yabancı', author: author, score: 6.9, isPublished: true};
+
 // ! means not nullable
 const typeDefs = gql`
+    type Author {
+        id: ID!
+        name: String!
+        surname: String
+        age: Int
+        books: [Book!]
+    }
+
     type Book {
         id: ID!
         title: String!
-        author: String
+        author: Author!
         score: Float
         isPublished: Boolean
     }
 
     type Query {
-        books: [Book]
+        book: Book
+        author: Author
     }
 `;
 
 const resolvers = {
 
     Query: {
-        books: () => [
-            {id: "sdk646dsad3d4sa4d", title: 'Yabancı', author: 'Albert Camus', score: 6.9, isPublished: true}
-        ],
+        book: () => book,
+        author: () => author,
     },
 };
 
